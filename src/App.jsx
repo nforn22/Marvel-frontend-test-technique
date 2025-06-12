b201f41dbf014619b600e8b2ec2ca006f291ae2e
nforn22 <fornarinicolas22@gmail.com>
2025-06-12 10:43:20 +0200

    feat: Add 'character' page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
 import './App.css'
+import Characters from './pages/Characters/Characters';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path="/characters" element={<Characters />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Characters/Characters.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Characters.css";
+
+function Characters() {
+  return (
+    <div className="characters-page">
+      <h1>Personnages Marvel</h1>
+      {/* contenu de la liste des characters ici */}
+    </div>
+  );
+}
+
+export default Characters; 
