d0669cad0b8e9b420ca917e332e1832604e8db49
nforn22 <fornarinicolas22@gmail.com>
2025-06-12 11:47:45 +0200

    feat: add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Characters from './pages/Characters/Characters';
 import Comics from './pages/Comics/Comics';
 import Favorites from './pages/Favorites/Favorites';
+import Home from './pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/characters" element={<Characters />} />
         <Route path="/comics" element={<Comics />} />
         <Route path="/favorites" element={<Favorites />} />
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Bienvenue sur le Marvel Explorer !</h1>
+      {/* contenu d'accueil ici */}
+    </div>
+  );
+}
+
+export default Home; 
